d189b06760073b3ebd036244ef326ea94e23da9e
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-07-08 13:48:32 -0300

    Adicionado base dos projetos à solução

---
+++ src/Services/PigeonBox/PigeonBox.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PigeonBox.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Services/PigeonBox/PigeonBox.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PigeonBox.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
