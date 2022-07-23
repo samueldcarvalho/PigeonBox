4e11e49b1b30093b0b16411dde34b1dd32904729
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-07-23 13:47:15 -0300

    Criação da tabview

--- src/UI/PigeonBox.Web.UI/src/components/ChatBox/index.tsx
+++ src/UI/PigeonBox.Web.UI/src/components/ChatBox/index.tsx
@@ -1,9 +1,10 @@
 /** @format */
-
-import ChatLateralMenuTabs from "../ChatLateralMenuTabs";
+import { useState } from "react";
 import styles from "./styles.module.css";
 
 const ChatBox = () => {
+  const [tabActive, setTabActive] = useState<"chats" | "contacts">("chats");
+
   return (
     <div className={styles.chatBoxFlexContainer}>
       <div className={styles.chatBoxContainer}>
@@ -14,13 +15,45 @@ const ChatBox = () => {
         </div>
         <div className={styles.chatBoxBody}>
           <div className={styles.chatBoxBodyLateralMenu}>
-            <ChatLateralMenuTabs />
+            <div className={styles.lateralMenuTabButtons}>
+              <button
+                className={
+                  tabActive == "chats"
+                    ? styles.activeTabButton
+                    : styles.tabButton
+                }
+                onClick={() => setTabActive("chats")}
+              >
+                Chats
+              </button>
+              <button
+                className={
+                  tabActive == "contacts"
+                    ? styles.activeTabButton
+                    : styles.tabButton
+                }
+                onClick={() => setTabActive("contacts")}
+              >
+                Contacts
+              </button>
+            </div>
+            <div>
+              {tabActive == "chats" ? <ContactsPanel /> : <ChatsPanel />}
+            </div>
           </div>
           <div className={styles.chatBoxBodyChatContainer}></div>
         </div>
       </div>
     </div>
   );
+};
+
+const ContactsPanel = () => {
+  return <>Contacts</>;
+};
+
+const ChatsPanel = () => {
+  return <>Chats</>;
 };
 
 export default ChatBox;
--- src/UI/PigeonBox.Web.UI/src/components/ChatLateralMenuTabs/index.tsx
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import styles from "./styles.module.css";
-
-const ChatLateralMenuTabs = () => {
-  return <div></div>;
-};
-
-export default ChatLateralMenuTabs;
