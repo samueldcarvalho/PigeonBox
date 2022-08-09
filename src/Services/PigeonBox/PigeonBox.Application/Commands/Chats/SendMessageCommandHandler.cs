ddcd87a6aa7e03b3cc703354f8fbdb9e37f1034f
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-08-08 23:09:51 -0300

    IoC do HubContext de ChatHub para notificar todos os clientes do chat que estão online para receberem as notificações da mensagem recebida.

--- src/Services/PigeonBox/PigeonBox.Application/Commands/Chats/SendMessageCommandHandler.cs
+++ src/Services/PigeonBox/PigeonBox.Application/Commands/Chats/SendMessageCommandHandler.cs
@@ -1,4 +1,7 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using PigeonBox.Application.Hubs;
+using PigeonBox.Application.Models.View;
 using PigeonBox.Core.CQRS;
 using PigeonBox.Domain.Interfaces;
 using PigeonBox.Domain.Messages;
@@ -14,16 +17,28 @@ namespace PigeonBox.Application.Commands.Chats
     public class SendMessageCommandHandler : CommandHandler, IRequestHandler<SendMessageCommand, CommandResponse<bool>>
     {
         private readonly IChatRepository _chatRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IHubContext<ChatHub> _hubContext;
 
-        public SendMessageCommandHandler(IChatRepository chatRepository)
+        public SendMessageCommandHandler(IChatRepository chatRepository, IHubContext<ChatHub> hubContext, IUserRepository userRepository)
         {
             _chatRepository = chatRepository;
+            _hubContext = hubContext;
+            _userRepository = userRepository;
         }
 
         public async Task<CommandResponse<bool>> Handle(SendMessageCommand request, CancellationToken cancellationToken)
         {
             if (!request.Validate())
                 return new CommandResponse<bool>(request.ValidationResult, false);
+
+            var user = await _userRepository.GetById(request.UserId);
+
+            if(user == null)
+            {
+                AddError("User cannot be finded");
+                return new CommandResponse<bool>(ValidationResult, false);
+            }
 
             var chat = await _chatRepository.GetById(request.ChatId);
 
@@ -40,6 +55,19 @@ namespace PigeonBox.Application.Commands.Chats
 
             _chatRepository.Update(chat);
             await _chatRepository.UnitOfWork.Commit();
+
+            await _hubContext.Clients.Clients(ChatHubHandler.UsersConnected
+                .Where(u => chat.Users.Any(user => user.Id == u.UserConnection.Id))
+                .Select(u => u.ConnectionId).ToList())
+                    .SendAsync("MessageReceived", new MessageViewModel
+                    {
+                        Id = message.Id,
+                        UserId = user.Id,
+                        UserName = user.Name,
+                        ChatId = message.ChatId,
+                        Text = message.Text,
+                        SentAt = message.SentAt
+                    });
 
             return new CommandResponse<bool>(ValidationResult, true);
         }
