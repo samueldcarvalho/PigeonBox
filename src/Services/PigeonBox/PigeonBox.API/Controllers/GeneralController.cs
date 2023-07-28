b3f96ea5886370c14a803ee00b7034c2c09c6d3b
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-07-28 00:32:07 -0300

    feat(api): geeral controller and life check

---
+++ src/Services/PigeonBox/PigeonBox.API/Controllers/GeneralController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace PigeonBox.API.Controllers
+{
+    [Route("/api/v1/[controller]")]
+    [ApiController]
+    public class GeneralController : ControllerBase
+    {
+        [HttpGet("check")]
+        public ActionResult<string> Check()
+        {
+            return Ok($"API Status: Online \nAt: { DateTime.Now }");
+        }
+    }
+}
